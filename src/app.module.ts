41c722308599d6152afd6010ee535ec7dfd8c17d
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2025-04-11 12:20:02 -0600

    asdf

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [AuthModule, UserModule, ConfigModule.forRoot({ isGlobal: true }), CommonModule],
 })
 export class AppModule {}
